82dde6162cc96fb63f82d97aaad15c34e1ebca21
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-14 10:56:03 +0300

    Добавление kdoc для утилитарных классов

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/core/domain/Result.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/core/domain/Result.kt
@@ -1,11 +1,37 @@
 package dev.filinhat.openlibapp.core.domain
 
+/**
+ * Обобщённый класс-обёртка для представления результата выполнения операции.
+ *
+ * @param D Тип данных в случае успешного результата.
+ * @param E Тип ошибки в случае неудачи. Должен быть подклассом [Error].
+ */
 sealed interface Result<out D, out E: Error> {
+
+    /**
+     * Класс для представления успешного результата.
+     *
+     * @param D Тип данных результата.
+     * @property data Данные успешного результата.
+     */
     data class Success<out D>(val data: D): Result<D, Nothing>
+
+    /**
+     * Класс для представления ошибки.
+     *
+     * @param E Тип ошибки, должен быть подклассом [Error].
+     * @property error Детали ошибки.
+     */
     data class Error<out E: dev.filinhat.openlibapp.core.domain.Error>(val error: E):
         Result<Nothing, E>
 }
 
+/**
+ * Преобразует успешный результат в новый результат с другим типом данных, применяя заданную функцию.
+ *
+ * @param map Функция преобразования данных.
+ * @return Новый результат с преобразованными данными или той же ошибкой.
+ */
 inline fun <T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
     return when(this) {
         is Result.Error -> Result.Error(error)
@@ -13,10 +39,21 @@ inline fun <T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
     }
 }
 
+/**
+ * Преобразует результат в пустой результат [EmptyResult], игнорируя данные.
+ *
+ * @return Пустой результат [EmptyResult] с той же ошибкой или успехом.
+ */
 fun <T, E: Error> Result<T, E>.asEmptyDataResult(): EmptyResult<E> {
     return map {  }
 }
 
+/**
+ * Выполняет заданное действие, если результат успешный.
+ *
+ * @param action Действие, выполняемое с данными успешного результата.
+ * @return Исходный результат, чтобы поддерживать цепочку вызовов.
+ */
 inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, E> {
     return when(this) {
         is Result.Error -> this
@@ -26,6 +63,13 @@ inline fun <T, E: Error> Result<T, E>.onSuccess(action: (T) -> Unit): Result<T, 
         }
     }
 }
+
+/**
+ * Выполняет заданное действие, если результат содержит ошибку.
+ *
+ * @param action Действие, выполняемое с данными ошибки.
+ * @return Исходный результат, чтобы поддерживать цепочку вызовов.
+ */
 inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E> {
     return when(this) {
         is Result.Error -> {
@@ -36,4 +80,9 @@ inline fun <T, E: Error> Result<T, E>.onError(action: (E) -> Unit): Result<T, E>
     }
 }
 
+/**
+ * Тип, представляющий пустой успешный результат или ошибку.
+ *
+ * @param E Тип ошибки.
+ */
 typealias EmptyResult<E> = Result<Unit, E>
--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/core/presentation/UiText.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/core/presentation/UiText.kt
@@ -4,17 +4,41 @@ import androidx.compose.runtime.Composable
 import org.jetbrains.compose.resources.StringResource
 import org.jetbrains.compose.resources.stringResource
 
-
+/**
+ * Интерфейс, представляющий текст, который может быть отображён в UI.
+ *
+ * Этот интерфейс поддерживает как динамические строки, так и строки из ресурсов.
+ */
 sealed interface UiText {
-    data class DynamicString(val value: String): UiText
+
+    /**
+     * Класс для представления динамической строки.
+     *
+     * @property value Текст строки.
+     */
+    data class DynamicString(val value: String) : UiText
+
+    /**
+     * Класс для представления строки из ресурсов.
+     *
+     * @property id Идентификатор строки из ресурсов.
+     * @property args Аргументы для форматирования строки (по умолчанию пустой массив).
+     */
     class StringResourceId(
         val id: StringResource,
         val args: Array<Any> = arrayOf()
-    ): UiText
+    ) : UiText
 
+    /**
+     * Преобразует экземпляр [UiText] в строку для отображения в интерфейсе.
+     *
+     * Эта функция использует аннотацию [Composable] для доступа к ресурсам строки в Compose.
+     *
+     * @return Текст в виде строки.
+     */
     @Composable
     fun asString(): String {
-        return when(this) {
+        return when (this) {
             is DynamicString -> value
             is StringResourceId -> stringResource(resource = id, formatArgs = args)
         }
