4e4f29ecd82220ec8945b4f3f99d9c033e357e13
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-20 18:36:34 +0300

    Реализован класс состояния экрана списка книг BookListState

---
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListState.kt
@@ -0,0 +1,23 @@
+package dev.filinhat.openlibapp.book.presentation.bookList
+
+import dev.filinhat.openlibapp.book.domain.Book
+import dev.filinhat.openlibapp.core.presentation.UiText
+
+/**
+ * Состояние списка книг.
+ *
+ * @property searchQuery Текст поискового запроса.
+ * @property searchResults Результаты поиска книг.
+ * @property favoriteBooks Список избранных книг.
+ * @property isLoading Индикатор загрузки данных.
+ * @property selectedTabIndex Индекс выбранной вкладки.
+ * @property erroeMessage Текст сообщения об ошибке.
+ */
+data class BookListState(
+    val searchQuery: String = "",
+    val searchResults: List<Book> = emptyList(),
+    val favoriteBooks: List<Book> = emptyList(),
+    val isLoading: Boolean = false,
+    val selectedTabIndex: Int = 0,
+    val erroeMessage: UiText? = null,
+)
