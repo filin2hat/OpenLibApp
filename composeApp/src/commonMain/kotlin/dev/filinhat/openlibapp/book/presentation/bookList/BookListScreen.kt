9dd7ccac962b41b5a1888f710b1e62bf8c140775
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-26 08:58:42 +0300

    Реализация TabRow в BookListScreen

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListScreen.kt
@@ -2,21 +2,37 @@ package dev.filinhat.openlibapp.book.presentation.bookList
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.filinhat.openlibapp.book.domain.Book
 import dev.filinhat.openlibapp.book.presentation.bookList.components.BookSearchBar
 import dev.filinhat.openlibapp.core.presentation.DarkBlue
+import dev.filinhat.openlibapp.core.presentation.DesertWhite
+import dev.filinhat.openlibapp.core.presentation.SandYellow
+import openlibrarycmpapp.composeapp.generated.resources.Res
+import openlibrarycmpapp.composeapp.generated.resources.favorites
+import openlibrarycmpapp.composeapp.generated.resources.search_results
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.viewmodel.koinViewModel
 
 /**
@@ -74,5 +90,74 @@ fun BookListScreen(
                     .fillMaxWidth()
                     .padding(16.dp),
         )
+
+        Surface(
+            modifier =
+                Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            color = DesertWhite,
+            shape =
+                RoundedCornerShape(
+                    topStart = 32.dp,
+                    topEnd = 32.dp,
+                ),
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier =
+                        Modifier
+                            .padding(vertical = 12.dp)
+                            .widthIn(max = 700.dp)
+                            .fillMaxWidth(),
+                    containerColor = DesertWhite,
+                    contentColor = SandYellow,
+                    indicator = { tabPositions ->
+                        TabRowDefaults.SecondaryIndicator(
+                            color = SandYellow,
+                            modifier =
+                                Modifier
+                                    .tabIndicatorOffset(tabPositions[state.selectedTabIndex]),
+                        )
+                    },
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f),
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.search_results),
+                            modifier = Modifier.padding(vertical = 12.dp),
+                        )
+                    }
+
+                    Tab(
+                        selected = state.selectedTabIndex == 1,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(1))
+                        },
+                        modifier = Modifier.weight(1f),
+                        selectedContentColor = SandYellow,
+                        unselectedContentColor = Color.Black.copy(alpha = 0.5f),
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.favorites),
+                            modifier = Modifier.padding(vertical = 12.dp),
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(4.dp))
+            }
+        }
     }
 }
