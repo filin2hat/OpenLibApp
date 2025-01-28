88cd9b15ba59477609688a03f66c922ffcf44a64
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-28 13:42:19 +0300

    Реализована логика загрузки и отображения книг в BookListViewModel.
    Добавлен метод observeSearchQuery для наблюдения за изменениями поискового запроса, а также логика для поиска и отображения результатов в зависимости от поискового запроса.

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListViewModel.kt
@@ -1,9 +1,17 @@
 package dev.filinhat.openlibapp.book.presentation.bookList
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.filinhat.openlibapp.book.domain.Book
 import dev.filinhat.openlibapp.book.domain.BookRepository
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 
 /**
@@ -12,6 +20,8 @@ import kotlinx.coroutines.flow.update
 class BookListViewModel(
     private val bookRepository: BookRepository,
 ) : ViewModel() {
+    private var cachedBooks: List<Book> = emptyList()
+
     private val _state = MutableStateFlow(BookListState())
     val state = _state.asStateFlow()
 
@@ -33,5 +43,32 @@ class BookListViewModel(
                 }
             }
         }
+    }
+
+    @OptIn(FlowPreview::class)
+    private fun observeSearchQuery() {
+        state
+            .map { it.searchQuery }
+            .distinctUntilChanged()
+            .debounce(500)
+            .onEach { query ->
+                when {
+                    query.isBlank() -> {
+                        _state.update {
+                            it.copy(
+                                errorMessage = null,
+                                searchResults = cachedBooks,
+                            )
+                        }
+                    }
+                    query.length >= 2 -> {
+                        searchBooks(query)
+                    }
+                }
+            }.launchIn(viewModelScope)
+    }
+
+    private fun searchBooks(query: String) {
+        TODO("Not yet implemented")
     }
 }
