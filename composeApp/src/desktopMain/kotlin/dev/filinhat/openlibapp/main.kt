86ad4155650b948b36e5447c617b7f9ba80b7f08
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-28 20:41:12 +0300

    Initialize HTTP Client engine in each platform
    Initialized HTTP Client engine with OkHttp for desktop and android platforms and Darwin for ios platform.

--- composeApp/src/desktopMain/kotlin/dev/filinhat/openlibapp/main.kt
+++ composeApp/src/desktopMain/kotlin/dev/filinhat/openlibapp/main.kt
@@ -1,13 +1,20 @@
+@file:Suppress("ktlint:standard:filename")
+
 package dev.filinhat.openlibapp
 
+import androidx.compose.runtime.remember
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import io.ktor.client.engine.okhttp.OkHttp
 
-fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "CMP-Bookpedia",
-    ) {
-        App()
+fun main() =
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "CMP-Bookpedia",
+        ) {
+            App(
+                engine = remember { OkHttp.create() },
+            )
+        }
     }
-}
