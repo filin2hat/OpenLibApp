858cc571e590e996758276a511a9bde80e8cf549
Бирюлин Дмитрий <dmitriy.biryulin@develonica.ru>
2025-01-28 15:51:42 +0300

    feat: Реализован поиск книг и обработка ошибок
    Реализован поиск книг по запросу пользователя с использованием BookRepository.
    Добавлена обработка ошибок при поиске книг и отображение сообщения об ошибке.
    Добавлена загрузка состояния при старте ViewModel и кэширование результатов поиска.
    Изменен способ обновления состояния с помощью `update`.
    Добавлена отмена предыдущего поиска при новом запросе.

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/BookListViewModel.kt
@@ -4,15 +4,22 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dev.filinhat.openlibapp.book.domain.Book
 import dev.filinhat.openlibapp.book.domain.BookRepository
+import dev.filinhat.openlibapp.core.domain.onError
+import dev.filinhat.openlibapp.core.domain.onSuccess
+import dev.filinhat.openlibapp.core.presentation.toUiText
 import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 /**
  * ViewModel для экрана списка книг.
@@ -21,9 +28,20 @@ class BookListViewModel(
     private val bookRepository: BookRepository,
 ) : ViewModel() {
     private var cachedBooks: List<Book> = emptyList()
+    private var searchJob: Job? = null
 
     private val _state = MutableStateFlow(BookListState())
-    val state = _state.asStateFlow()
+    val state =
+        _state
+            .onStart {
+                if (cachedBooks.isEmpty()) {
+                    observeSearchQuery()
+                }
+            }.stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(5000L),
+                _state.value,
+            )
 
     fun onAction(action: BookListAction) {
         when (action) {
@@ -61,14 +79,37 @@ class BookListViewModel(
                             )
                         }
                     }
+
                     query.length >= 2 -> {
-                        searchBooks(query)
+                        searchJob?.cancel()
+                        searchJob = searchBooks(query)
                     }
                 }
             }.launchIn(viewModelScope)
     }
 
-    private fun searchBooks(query: String) {
-        TODO("Not yet implemented")
-    }
+    private fun searchBooks(query: String) =
+        viewModelScope.launch {
+            _state.update { it.copy(isLoading = true) }
+            bookRepository
+                .searchBooks(query)
+                .onSuccess { searchResults ->
+                    cachedBooks = searchResults
+                    _state.update {
+                        it.copy(
+                            searchResults = searchResults,
+                            errorMessage = null,
+                            isLoading = false,
+                        )
+                    }
+                }.onError { error ->
+                    _state.update {
+                        it.copy(
+                            searchResults = emptyList(),
+                            isLoading = false,
+                            errorMessage = error.toUiText(),
+                        )
+                    }
+                }
+        }
 }
