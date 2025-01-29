f1fe7621e91987ccd6ec98a25d1dd00411d78313
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-29 19:08:45 +0300

    feat: add DI modules
    Add DI modules for Android, iOS, Desktop and common modules.
    - Implemented platform-specific modules for HttpClientEngine.
    - Added shared module with Ktor setup, data sources, repositories and ViewModel.

---
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/di/Modules.kt
@@ -0,0 +1,25 @@
+package dev.filinhat.openlibapp.di
+
+import dev.filinhat.openlibapp.book.data.network.KtorRemoteBookDataSource
+import dev.filinhat.openlibapp.book.data.network.RemoteBookDataSource
+import dev.filinhat.openlibapp.book.data.repository.DefaultBookRepository
+import dev.filinhat.openlibapp.book.domain.BookRepository
+import dev.filinhat.openlibapp.book.presentation.bookList.BookListViewModel
+import dev.filinhat.openlibapp.core.data.network.HttpClientFactory
+import org.koin.core.module.Module
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+expect val platformModule: Module
+
+val sharedModule =
+    module {
+        single { HttpClientFactory.create(get()) }
+
+        singleOf(::KtorRemoteBookDataSource).bind<RemoteBookDataSource>()
+        singleOf(::DefaultBookRepository).bind<BookRepository>()
+
+        viewModelOf(::BookListViewModel)
+    }
