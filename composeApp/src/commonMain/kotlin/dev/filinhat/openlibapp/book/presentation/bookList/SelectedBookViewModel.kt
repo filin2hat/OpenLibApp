d8d89bff8f58004cd71d46b9cd1c3bbf1b56f73b
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-01-30 11:12:14 +0300

    Implemented SelectedBookViewModel for managing the selected book state.

---
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/SelectedBookViewModel.kt
@@ -0,0 +1,15 @@
+package dev.filinhat.openlibapp.book.presentation.bookList
+
+import androidx.lifecycle.ViewModel
+import dev.filinhat.openlibapp.book.domain.Book
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class SelectedBookViewModel : ViewModel() {
+    private val _selectedBook = MutableStateFlow<Book?>(null)
+    val selectedBook = _selectedBook.asStateFlow()
+
+    fun onSelectBook(book: Book?) {
+        _selectedBook.value = book
+    }
+}
