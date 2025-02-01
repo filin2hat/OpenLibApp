8655567c9496714823e3e6913dc0a2d9e309bf9e
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-02-01 20:43:04 +0300

    Implement book description retrieval in BookRepository and DefaultBookRepository.

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookDetail/BookDetailViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookDetail/BookDetailViewModel.kt
@@ -1,13 +1,30 @@
 package dev.filinhat.openlibapp.book.presentation.bookDetail
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
+import dev.filinhat.openlibapp.app.Route
+import dev.filinhat.openlibapp.book.domain.BookRepository
+import dev.filinhat.openlibapp.core.domain.onSuccess
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class BookDetailViewModel : ViewModel() {
+class BookDetailViewModel(
+    private val bookRepository: BookRepository,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
     private val _state = MutableStateFlow(BookDetailState())
-    val state = _state.asStateFlow()
+    val state =
+        _state
+            .onStart { fetchBookDescription() }
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000L), _state.value)
+
+    private val bookId = savedStateHandle.toRoute<Route.BookDetail>().id
 
     fun onAction(action: BookDetailAction) {
         when (action) {
@@ -17,13 +34,26 @@ class BookDetailViewModel : ViewModel() {
 
             is BookDetailAction.OnSelectedBookChange -> {
                 _state.update {
-                    it.copy(
-                        book = action.book,
-                    )
+                    it.copy(book = action.book)
                 }
             }
 
             else -> Unit
+        }
+    }
+
+    private fun fetchBookDescription() {
+        viewModelScope.launch {
+            bookRepository
+                .getBookDescription(bookId)
+                .onSuccess { description ->
+                    _state.update {
+                        it.copy(
+                            book = it.book?.copy(description = description ?: ""),
+                            isLoading = false,
+                        )
+                    }
+                }
         }
     }
 }
