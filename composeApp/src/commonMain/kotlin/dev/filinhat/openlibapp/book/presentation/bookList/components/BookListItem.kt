443a67ad5252d2a2558ca43cd79c9a1b2f2cd4a0
Dmitriy Biryulin <biryulin.develop@gmail.com>
2025-02-04 08:41:55 +0300

    Реализация анимации отображения обложки книги

--- composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/components/BookListItem.kt
+++ composeApp/src/commonMain/kotlin/dev/filinhat/openlibapp/book/presentation/bookList/components/BookListItem.kt
@@ -1,5 +1,7 @@
 package dev.filinhat.openlibapp.book.presentation.bookList.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -27,10 +29,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImagePainter
 import coil3.compose.rememberAsyncImagePainter
 import dev.filinhat.openlibapp.book.domain.Book
 import dev.filinhat.openlibapp.core.presentation.LightBlue
@@ -92,6 +97,12 @@ fun BookListItem(
                         },
                     )
 
+                val painterState by painter.state.collectAsStateWithLifecycle()
+                val transition by animateFloatAsState(
+                    targetValue = if (painterState is AsyncImagePainter.State.Success) 1f else 0f,
+                    animationSpec = tween(durationMillis = 800),
+                )
+
                 when (val result = imageLoaderResult) {
                     null ->
                         PulseAnimation(modifier = Modifier.size(60.dp))
@@ -106,7 +117,12 @@ fun BookListItem(
                                     .aspectRatio(
                                         ratio = 0.65f,
                                         matchHeightConstraintsFirst = true,
-                                    ),
+                                    ).graphicsLayer {
+                                        rotationX = (1f - transition) * 60
+                                        val scale = 0.8f + (0.2f * transition)
+                                        scaleX = scale
+                                        scaleY = scale
+                                    },
                         )
                     }
                 }
